cb945f23d02ececc729edc7f53ceaa800a3c6003
vinay2498 <71204997+vinay2498@users.noreply.github.com>
2020-09-16 12:34:45 +0530

    Update telegram_bot_adafruit1.py

--- telegram_bot_adafruit1.py
+++ telegram_bot_adafruit1.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters   
 from Adafruit_IO import Client,Data
 import os
 
